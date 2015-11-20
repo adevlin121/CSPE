922b06cdd311190822aa867ac5764d06de7473b9
Aidan Devlin <Aidan@Aidans-MacBook-Pro-2.local>
2015-11-20 17:18:01 +0000

    shane is stupid

--- intro/daytimeclient.c
+++ intro/daytimeclient.c
@@ -19,3 +19,30 @@ main(int argc, char **argv)
     
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family == AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    {
+        err_quit("inet_pton error for %s", argv[1]);
+    }
+    if(connect (sockfd,  (SA *) & servaddr, sizeof(servaddr)) < 0)
+    {
+        err_sys("connect error");
+    }
+    while( (n = read(sockfd, recvline, MAXLINE)) > 0)
+    {
+        counter++;
+        recvline[n]=0;
+        if(fputs(recvline, stdout) == EOF)
+        {
+            err_sys("fputs error");
+        }
+    }
+    if(n < 0)
+    {
+            err_sys("read error");
+    }
+
+    printf("counter = %d\n", counter);
+
+    exit(0);
+}
